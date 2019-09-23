db122322bfe29b81da65b162260d820be041ca69
EdGunUk <edik95mt@gamil.com>
2019-09-23 15:05:28 +0300

    save chenges

--- js/header.js
+++ js/header.js
@@ -5,8 +5,6 @@ let iconHamburger = document.body.querySelector(".icon-hamburger");
 function toggleClass() {
   menu.classList.toggle("menu_close");
   iconHamburger.classList.toggle("icon-hamburger_cross");
-  
-  event.preventDefault();
 }
 
 iconHamburger.addEventListener("click", toggleClass);
