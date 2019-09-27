2e161e83cbd43992f27ef8d86a0459f3399aeb46
EdGunUk <edik95mt@gamil.com>
2019-09-27 22:45:58 +0300

    add preloader

---
+++ js/preloader.js
@@ -0,0 +1,7 @@
+"use strict";
+
+window.onload = function() {
+  document.body.querySelector(".preloader").style.display = "none";
+  document.body.style.overflow = "visible";
+  document.documentElement.style.overflow = "visible";
+};
